519c0c453f45a8f6a61838926020c42e9d5131e2
mcfilu <filip.1olszewski@gmail.com>
2023-07-31 18:11:17 +0100

    test

--- src/components/OconceptDetails.jsx
+++ src/components/OconceptDetails.jsx
@@ -65,6 +65,7 @@ unique protocols designed for each patient’s condition.</p>
         
 
         <div className={`flex flex-col md:hidden items-center`}>
+            <video src={o_concept_animation} autoPlay loop muted playsInline className={`w-[100%]`}></video>
             <p className={`flex text-gold1 text-[6vh] text-center font-header`}>How does it work?</p>
         
              {/* <video ref={videoRef} src={o_concept_animation} autoPlay loop muted playsInline webkit-playsInline className={`w-[100%]`}></video> */}
